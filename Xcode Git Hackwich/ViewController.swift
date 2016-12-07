6b0a589140955b397547fd7fae8d675eacb96dff
King Jack <student@jmoran2.local>
2016-12-07 11:13:41 -0600

    I made a button

--- Xcode Git Hackwich/ViewController.swift
+++ Xcode Git Hackwich/ViewController.swift
@@ -14,10 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onButtonTapped(_ sender: UIButton) {
+        print("button was tapped")
     }
 
 
